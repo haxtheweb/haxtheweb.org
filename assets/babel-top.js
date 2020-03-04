b5eecdc5becaaccdb0aa0d5f0264caccadae74eb
DDEV-Local User <nobody@example.com>
2020-03-04 15:01:28 +0000

    Manifest updated

--- assets/babel-top.js
+++ assets/babel-top.js
@@ -1,1 +1,5 @@
+XSym
+0027
+a4c9a074e8dd652401c07ba5bdbcf84d
 ../../../babel/babel-top.js
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
